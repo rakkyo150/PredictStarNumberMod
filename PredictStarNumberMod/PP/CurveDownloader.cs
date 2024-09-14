2b1e09f445e00ea1e3d998847ee4fcc86596974f
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2024-09-15 05:15:12 +0900

    PP計算まで実装(modifierは非対応、Conflict解消)

---
+++ PredictStarNumberMod/PP/CurveDownloader.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using PredictStarNumberMod.HarmonyPatches;
+using System;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using Zenject;
+using static PredictStarNumberMod.PP.PPCalculatorData;
+
+namespace PredictStarNumberMod.PP
+{
+    internal class CurveDownloader: IInitializable
+    {
+        private const string URI_PREFIX = "https://cdn.pulselane.dev/";
+        private const string CURVE_FILE_NAME = "curves.json";
+
+        private static HttpClient _client;
+        private static HttpClient Client
+        {
+            get
+            {
+                if (_client == null)
+                {
+                    Connect();
+                }
+
+                return _client;
+            }
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                LevelStatsViewPatch.CurvesDownloadFinished = false;
+                GetCurves();
+            }
+            catch (Exception)
+            {
+                Plugin.Log.Error("Failed to initialize CurveDownloader");
+            }
+        }
+
+        private async Task GetCurves()
+        {
+            string uri = URI_PREFIX + CURVE_FILE_NAME;
+            Curves result = await this.MakeWebRequest<Curves>(uri);
+            LevelStatsViewPatch.Curves = result;
+            LevelStatsViewPatch.CurvesDownloadFinished = true;
+        }
+
+        private async Task<T> MakeWebRequest<T>(string uri)
+        {
+            HttpResponseMessage result = await Client.GetAsync(uri);
+            if (result == null || !result.IsSuccessStatusCode)
+            {
+                Plugin.Log?.Error($"Failed to download {uri}");
+                return default;
+            }
+            string jsonString = await result.Content.ReadAsStringAsync();
+            Plugin.Log?.Info(jsonString);
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+
+        private static void Connect()
+        {
+            _client = new HttpClient()
+            {
+                Timeout = new TimeSpan(0, 0, 15)
+            };
+            _ = _client.DefaultRequestHeaders.UserAgent.TryParseAdd($"{Assembly.GetExecutingAssembly().GetName().Name}/{Assembly.GetExecutingAssembly().GetName().Version}");
+        }
+    }
+}
---
+++ PredictStarNumberMod/PP/PPCalculatorData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PredictStarNumberMod.PP
+{
+    public class PPCalculatorData
+    {
+        public static double DefaultStarMultipllier { get; } = 42.11;
+
+        public class Curves
+        {
+            public ScoreSaber ScoreSaber { get; set; }
+        }
+
+        public class ScoreSaber
+        {
+            public List<Point> modifierCurve { get; set; }
+            public List<Point> standardCurve { get; set; }
+            public List<string> songsAllowingPositiveModifiers { get; set; }
+            public ScoreSaberModifiers modifiers { get; set; }
+        }
+
+        public class Point
+        {
+            public float x { get; set; }
+            public float y { get; set; }
+        }
+
+        public class ScoreSaberModifiers
+        {
+            public float da;
+            public float gn;
+            public float fs;
+        }
+    }
+}
