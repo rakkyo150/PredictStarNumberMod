f4607ba26a73616a40311a98fe1b3e266a2e39da
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2025-10-16 04:33:20 +0900

    Rustのライブラリの名前を調整

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     csbindgen::Builder::default()
         .input_extern_file("src/lib.rs")
-        .csharp_dll_name("Libs/predict_star_number_mod.dll")
+        .csharp_dll_name("Libs/PredictStarNumberModLib.dll")
         .csharp_use_nint_types(false)
         .generate_csharp_file("PredictStarNumberMod/Star/Predict.cs")
         .unwrap();
