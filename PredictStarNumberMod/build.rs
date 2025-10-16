827b179e7bee7c267aaccaf5df4d9336b2e200e7
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2025-10-16 22:47:13 +0900

    Rustの実行ファイルの場所を変更して、C#のビルド時にdllを出力先にコピーするようにした

--- PredictStarNumberMod/build.rs
+++ PredictStarNumberMod/build.rs
@@ -3,6 +3,6 @@ fn main() {
         .input_extern_file("src/lib.rs")
         .csharp_dll_name("Libs/PredictStarNumberModLib.dll")
         .csharp_use_nint_types(false)
-        .generate_csharp_file("PredictStarNumberMod/Star/Predict.cs")
+        .generate_csharp_file("Star/Predict.cs")
         .unwrap();
 }
